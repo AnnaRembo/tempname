f8e89b5717dbe809f8c4675a6c84eb0fdd90cad7
AnnaRembo <Rambo-o@yandex.ru>
2020-06-12 20:32:31 +0300

    Update getCpuUsage.ps1

--- getCpuUsage.ps1
+++ getCpuUsage.ps1
@@ -1,4 +1,3 @@
 $parameter = $args[0]
 
-#echo 'Map of process ID to CPU usage:'
 Get-WmiObject -Query "Select * from Win32_PerfFormattedData_PerfProc_Process" | Select-Object -Property IDProcess,Name,PercentProcessorTime | Where-Object {$_.IDProcess -eq $parameter} | ConvertTo-Csv -NoTypeInformation
